e9e81ed02d9c795e5772f893fec2edae8fc3634a
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2023-05-13 12:41:14 +0530

    Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
-export default class Home extends Component {
-  render() {
+export default function Home(){
+
+  const [body, setBody] = useState();
+  useEffect(()=>{
+    const baseURL = "https://api.instagram.com/oauth/authorize?client_id=773946244319476&redirect_uri=https://getit-profile-attack.vercel.app/auth/login&scope=user_profile,user_media&response_type=code";
+    axios.get(baseURL).then((res)=> {
+      setBody(res)
+    })
+  })
     return (
-      <div>Home</div>
+      <>
+        <div>{body}</div>
+      </>
     )
   }
-}
+
